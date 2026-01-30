4e41b7f266a403a5c93b4dbef63d214df84c9573
Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>
2026-01-30 19:21:01 +0100

    feat(models): add AWSAccountContext for OU path and tags

--- src/adapters/aws_context.py
+++ src/adapters/aws_context.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
-from typing import List
+from typing import List, Dict
 
 @dataclass
 class AWSAccountContext:
     ou_path_ids: List[str] = field(default_factory=list)
+    tags: Dict[str, str] = field(default_factory=dict)
