b137e2f5b6b8c9f979e015c2e9e7b1503d6ce711
Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>
2026-02-01 17:41:13 +0100

    feat(adapters): add get_ou_path signature

--- src/adapters/aws_orgs.py
+++ src/adapters/aws_orgs.py
@@ -6,5 +6,8 @@ from src.models.aws_context import AWSAccountContext
 class AWSOrganizationsAdapter:
     def __init__(self, orgs_client=None): 
         self.orgs = orgs_client or boto3.client("organizations")
+    def get_ou_path(self, account_id: str) -> List[str]:
+        
+
 
 
