860264956be9efe528afb355c83aa59411cf7b12
Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>
2026-02-02 13:25:51 +0100

    chore: removed unnecessary import

--- src/core/workflow.py
+++ src/core/workflow.py
@@ -1,6 +1,5 @@
 from src.core.engine import EvaluationResult
 from src.models.request import AccessRequest
-from src.models.aws_context import AWSAccountContext
 from src.core.engine import PolicyEngine
 from src.adapters.aws_orgs import AWSOrganizationsAdapter
 
