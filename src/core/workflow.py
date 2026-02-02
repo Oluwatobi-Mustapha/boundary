8ac5c493b8642269616c57739444f252c208ef5f
Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>
2026-02-02 13:18:44 +0100

    feat(workflow): define dependencies and imports

---
+++ src/core/workflow.py
@@ -0,0 +1,6 @@
+from src.core.engine import EvaluationResult
+from src.models.request import AccessRequest
+from src.models.aws_context import AWSAccountContext
+from src.core.engine import PolicyEngine
+from src.adapters.aws_orgs import AWSOrganizationsAdapter
+
