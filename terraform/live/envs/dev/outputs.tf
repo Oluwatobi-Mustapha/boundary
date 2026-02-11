f54663d3f8545f991eed3c8237938c1f28f5a22d
Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>
2026-02-11 04:17:11 +0100

    feat(dev): connect boundary state module to outputs.tf

--- terraform/live/envs/dev/outputs.tf
+++ terraform/live/envs/dev/outputs.tf
@@ -7,3 +7,8 @@ output "group_ids" {
   description = "Group IDs (Update config/access_rules.yaml with these)"
   value       = module.boundary_identity.group_ids
 }
+
+output "dynamodb_table_name" {
+  description = "The DynamoDB Table for State Persistence"
+  value       = module.boundary_state.table_name
+}
