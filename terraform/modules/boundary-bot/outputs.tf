e2f084a77658fb018f4a003d9447ed9ee9d0c854
Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>
2026-02-17 18:31:22 +0100

    chore(terraform): expose janitor Lambda ARN and execution role name

---
+++ terraform/modules/boundary-bot/outputs.tf
@@ -0,0 +1,9 @@
+output "lambda_function_arn" {
+  description = "ARN of the Janitor Lambda Function"
+  value       = aws_lambda_function.janitor.arn
+}
+
+output "lambda_role_name" {
+  description = "Name of the IAM Role used by the Janitor"
+  value       = aws_iam_role.janitor_execution.name
+}
