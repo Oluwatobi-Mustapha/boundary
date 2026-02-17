45629b6f2b6186fe731de2a5c777fc753e2453f2
Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>
2026-02-17 20:28:03 +0100

    feat(boundary-bot): update the outputs.tf for Boundary alerts sns topic

--- terraform/modules/boundary-bot/outputs.tf
+++ terraform/modules/boundary-bot/outputs.tf
@@ -7,3 +7,9 @@ output "lambda_role_name" {
   description = "Name of the IAM Role used by the Janitor"
   value       = aws_iam_role.janitor_execution.name
 }
+
+# --- NEW OUTPUT ---
+output "sns_topic_arn" {
+  description = "SNS Topic for Boundary Alerts (Subscribe your email here)"
+  value       = aws_sns_topic.alerts.arn
+}
