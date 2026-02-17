b8f4a58c70a03fe70f1f6ffe213028a06734a14e
Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>
2026-02-17 18:55:26 +0100

    feat(boundary-identity): update boundary identity to output SSO details

--- terraform/modules/boundary-identity/outputs.tf
+++ terraform/modules/boundary-identity/outputs.tf
@@ -7,3 +7,15 @@ output "group_ids" {
   description = "Map of Group Names to their Identity Store IDs"
   value       = { for k, v in aws_identitystore_group.this : k => v.group_id }
 }
+
+# --- NEW OUTPUTS FOR THE BOT ---
+
+output "sso_instance_arn" {
+  description = "The ARN of the SSO Instance found by this module"
+  value       = local.sso_instance_arn
+}
+
+output "identity_store_id" {
+  description = "The Identity Store ID found by this module"
+  value       = local.identity_store_id
+}
